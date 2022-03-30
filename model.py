0054488421d3ab1f848ecfc811175fcdf1c0c53a
Manuel <neto.map@gmail.com>
2022-03-30 16:19:24 -0300

    melhorias no script de model e model_architecture. Para faciliar o carregamento automático

--- model.py
+++ model.py
@@ -15,40 +15,53 @@ class YOLO(nn.Module):
         self.IMG_SIZE = IMG_SIZE
         self.architecture_config = architecture_config
 
-        backbone_blocks = []
-        for block in self.architecture_config:
-            backbone_blocks.append(self.create_block(block))
+        blocks = self.create_blocks()
+        self.net = nn.Sequential(*blocks)
 
-        backbone_blocks.append(nn.Flatten())
-        backbone = nn.Sequential(*backbone_blocks)
-
-        input_test = torch.rand((1, 3, self.IMG_SIZE, self.IMG_SIZE))
-        output = backbone(input_test)
-        in_features_linear = output.shape[-1]
-
-        self.net = nn.Sequential(
-            backbone, 
-            nn.Linear(in_features=in_features_linear, out_features=1024*2),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=1024*2, out_features=1024),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=1024, out_features=(self.S * self.S * (1 + 4 + self.C)))
-        )
-    
-    def create_block(self, block):
+    def create_blocks(self):
         
-        block_type = block.__class__.__name__
-        if (block_type == 'list'): # é uma camada convolucional
-            in_channels, out_channels, kernel_size, stride, padding = block
-            return nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        elif (block_type == 'str'): # pode ser leaky ou maxpool
-            layer_type, value = block.split(':')
-            if (layer_type == 'maxpool'):
-                return nn.MaxPool2d(kernel_size=int(value))
-            elif (layer_type == 'leaky'):
-                return nn.LeakyReLU(negative_slope=float(value))
-            else:
-                raise "Nome de camada não reconhecida! Avaliar a estrutura novamente."
+        backbone_blocks = []
+        camada_apos_flatten = False
+        in_features_depois_flatten = -1
+
+        for block in self.architecture_config: # para cada bloco da arquitetura
+
+            block_type = block.__class__.__name__ # pega o nome do tipo do bloco. Lista ou string
+
+            if (block_type == 'list'): # é uma lista (pode ser conv ou linear)
+                if (len(block) == 5): # é uma camada convolucional
+                    in_channels, out_channels, kernel_size, stride, padding = block
+                    backbone_blocks.append(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)) # adiciona convolucional
+                elif (len(block) == 2): # é linear
+                    in_features, out_features = block  # só tem dois elementos
+                    if (camada_apos_flatten):  # avisa se essa layer é justamente após a camada flatten, para calcular o número de neurônios da entrada
+                        in_features = in_features_depois_flatten
+                        camada_apos_flatten = False # a partir dessa camada, já não é mais após a flatten. 
+                    
+                    if (out_features == -1):   # como padrão, inserir -1 nos campos que devem ser calculados, como neste caso que é a saída de 
+                        out_features = self.S * self.S * (1 + 4 + self.C)  # neurônios da rede YOLO.
+
+                    backbone_blocks.append(nn.Linear(in_features, out_features))  # adiciona os valores calculados e/ou digitados.
+                else:
+                    raise "lista com valores diferente de 5 (conv) ou 2 (linear)"
+
+            elif (block_type == 'str'): # pode ser leaky ou maxpool
+                layer_type, value = block.split(':')
+                if (layer_type == 'maxpool'):
+                    backbone_blocks.append(nn.MaxPool2d(kernel_size=int(value))) # adiciona maxpool
+                elif (layer_type == 'leaky'):
+                    backbone_blocks.append(nn.LeakyReLU(negative_slope=float(value))) # adiciona leakyRELU
+                elif (layer_type == 'flatten'):
+                    backbone_blocks.append(nn.Flatten())
+                    model_test = nn.Sequential(*backbone_blocks)                    # cria um modelo teste rapidamente
+                    input_test = torch.rand((1, 3, self.IMG_SIZE, self.IMG_SIZE))   # simula uma entrada simples
+                    output = model_test(input_test)                                 # calcula a saída
+                    in_features_depois_flatten = output.shape[-1]                   # pega o shape da saída flatten para servir de entrada nas camadas lineares
+                    camada_apos_flatten = True      # marca que essa camada é flatten e a próxima é True
+                else:
+                    raise "Nome de camada não reconhecida! Avaliar a estrutura novamente."
+        
+        return backbone_blocks
     
     def forward(self, x):
         return self.net(x)
@@ -57,6 +70,7 @@ if __name__ == '__main__':
 
     ma = model_architeture()
     model = YOLO(4, 2, 1, 300, ma.architecture_config)
-    input = torch.rand((2, 3, 300, 300))
-    output = model(input)
-    print (f'output.shape: {output.shape}')
+    input_test = torch.rand((1, 3, 300, 300))
+    output = model(input_test)
+    print (model)
+    print (output.shape)
--- model_architecture.py
+++ model_architecture.py
@@ -1,26 +1,35 @@
 class model_architeture():
 
     def __init__(self):
+        CONVOLUCIONAL = [3, 32, 3, 1, 0]
+        LEAKY_RELU = 'leaky:.1'
+        MAX_POOL = 'maxpool:2'
+
         self.architecture_config = [
-            [3, 128, 5, 1, 0],
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            MAX_POOL,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            MAX_POOL,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            MAX_POOL,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            MAX_POOL,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            CONVOLUCIONAL,
+            LEAKY_RELU,
+            'flatten:',
+            [-1, 1024*2], # LINEAR a entrada é calculada pelo modelo
             'leaky:.1',
-            [128, 128, 5, 1, 0],
+            [1024*2, 1024],
             'leaky:.1',
-            'maxpool:2',
-            [128, 128, 5, 1, 0],
-            'leaky:.1',
-            [128, 64, 5, 1, 0],
-            [64, 64, 9, 1, 0],
-            'leaky:.1',
-            'maxpool:2',
-            [64, 64, 5, 1, 0],
-            'leaky:.1',
-            'maxpool:2',
-            [64, 64, 7, 1, 0],
-            'leaky:.1',
-            'maxpool:2',
-            [64, 64, 5, 1, 0],
-            'leaky:.1',
-            [64, 64, 5, 1, 0],
-            'leaky:.1',
+            [1024, -1]    # LINEAR a saída é calculada pelo modelo
         ]
